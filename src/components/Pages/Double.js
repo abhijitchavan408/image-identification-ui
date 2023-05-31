814f4f1f2181299180af5923862feff99889e23d
Chavan <abhijit-sambhaji.chavan@hpe.com>
2023-05-31 14:40:09 +0530

    trail commit for ci/cd

--- src/components/Pages/Double.js
+++ src/components/Pages/Double.js
@@ -1,9 +1,105 @@
-import React from "react";
+
+import React, { useRef } from "react";
+import { useState } from "react";
+import SingleService from "../../service/SingleService";
+import "../../css/NavBar.css"
+import "../../css/Single.css"
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { ImCheckmark, ImCross } from "react-icons/im";
+
 
 export const Double = () => {
+
+  const percentage = 66;
+
+  const [defaultImg, setDefaultImg] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
+  const inputRef = useRef();
+  const [img, setImg] = useState([]);
+  const [status, setStatus] = useState(false)
+  const [response, setResponse] = useState(" ")
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  }
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setImg(event.dataTransfer.files[0]);
+    setDefaultImg(URL.createObjectURL(event.dataTransfer.files[0]))
+
+  }
+  const formData = new FormData();
+  formData.append('image', img);
+
+
+  const uploadImage = () => {
+    setStatus(true);
+    SingleService.uploadImage(formData).then(Response => {
+
+      console.log(Response.data);
+      setResponse(Response.data.identity)
+    })
+      .catch(error => {
+        // Handle error
+        console.log("Error uploading image:", error);
+      });
+  }
+  const correct = () => {
+    SingleService.feedback("correct")
+  }
+
+  const wrong = () => {
+    SingleService.feedback("wrong")
+  }
+
+  const result = () => {
+
+  }
+
+
   return (
-    <div>
-      <h1>Double</h1>
-    </div>
+    <div onDragOver={handleDragOver}
+      onDrop={handleDrop} className="container">
+      <div className="row row-cols-auto">
+        <div className="col"></div>
+        <div className="col">
+          <img src={defaultImg} alt="Cinque Terre" className="rounded-circle image" />
+        </div>
+        {status &&
+          <div className="box outerbox" style={{ textAlign: "center" }}>
+            {/* <CircularProgressbar className="percentage" value={percentage} text={`${percentage}%`} />
+        <h4>Human Probability</h4> */}
+            <h3>{response}</h3>
+            <p> <span onClick={correct}><ImCheckmark style={{ height: "30px" }} /></span>
+              <span onClick={wrong}><ImCross style={{ height: "25px" }} /></span>
+            </p>
+          </div>
+        }
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <div class="btn-group" role="group" aria-label="Basic example">
+            <button type="file" className=" btn btn-dark show col-6" onClick={() => inputRef.current.click()}>Upload Image</button>
+          </div>
+          <button type="button" className="btn btn-primary show" onClick={uploadImage}>Submit </button>
+        </div>
+      </div>
+      <div>
+        &nbsp;
+        <label className="">
+          <input type="file"
+            name="image"
+            onChange={(e) => {
+              setImg(e.target.files[0]);
+              setDefaultImg(URL.createObjectURL(e.target.files[0]))
+            }}
+            hidden
+            ref={inputRef} />
+
+        </label>
+        {/* <button type="file" className=" btn btn-dark show" onClick={() => inputRef.current.click()}>Upload Image</button> */}
+
+      </div>
+    </div >
   );
 };
